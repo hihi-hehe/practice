a563d88caf5b3834bb9509c57e186b11435596e2
hihihehe <gothy0117@gmail.com>
2021-03-17 04:51:42 +0000

    download train datasets

--- MLP.py
+++ MLP.py
@@ -12,3 +12,5 @@ else :
 print('Using PyTorch version: ', torch.__version__, '\t Device: ', DEVICE)
 BATCH_SIZE = 32
 EPOCHS = 10
+
+train_dataset = datasets.MNIST(root="/data/MNIST", train = True, download = True, transform = transforms.ToTensor())
